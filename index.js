c9ab302ffb90af7dc2cf748d54b8af2022d93ca2
Ubuntu <ubuntu@ip-172-31-10-45.us-west-1.compute.internal>
2016-03-18 02:19:55 +0000

    CMPE 172 HW 2 Part 2 - Kevin Lai

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ console.log("Loaded data after finish reading index.html:\n" + data);
 */
 
 /* Part 1b */
-
+/*
 fs.readFile('./index.html', function(err, data) {
     if(err) {
         throw err;
@@ -40,3 +40,34 @@ fs.readFile('./index.html', function(err, data) {
 });
 
 console.log("Listening to server at http://localhost:8080\n");
+*/
+
+/* Part 2 */
+
+fs.stat('./index.html', function(err, stats) {
+
+    fs.open('./index.html', 'r', function(err, file) {
+
+        var buffer = new Buffer(stats.size);
+
+        fs.read(file, buffer, 0, buffer.length, null, function(err, bytesRead, buffer) {
+            var data = buffer.toString();
+
+            http.createServer(function(request, response) {
+
+                response.writeHead(200, {"Context-Type": "text/html"});
+                response.write(data);
+                response.end();
+
+            }).listen(process.env.PORT || 8080);
+
+            console.log("Loaded data after finish reading index.html:\n" + data);
+
+            fs.close(file);
+        });
+    });
+
+});
+
+console.log("Listening to server at http://localhost:8080\n");
+
