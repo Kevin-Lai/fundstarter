d02dd05c6392da4ef06ce2fd4fd9d3c32351e4d8
Ubuntu <ubuntu@ip-172-31-10-45.us-west-1.compute.internal>
2016-03-18 02:16:08 +0000

    CMPE 172 HW 2 Part 1b - Kevin Lai

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http');
 var fs = require('fs');
 
 /* Part 1a */
-
+/*
 var data = fs.readFileSync('./index.html');
 
 http.createServer(function(request, response) {
@@ -18,4 +18,25 @@ http.createServer(function(request, response) {
 console.log("Listening to server at http://localhost:8080\n");
 
 console.log("Loaded data after finish reading index.html:\n" + data);
+*/
 
+/* Part 1b */
+
+fs.readFile('./index.html', function(err, data) {
+    if(err) {
+        throw err;
+    }
+
+    http.createServer(function(request, response) {
+
+        response.writeHead(200, {"Context-Type": "text/html"});
+        response.write(data);
+        response.end();
+
+    }).listen(process.env.PORT || 8080);
+
+    console.log("Loaded data after finish reading index.html:\n" + data);
+
+});
+
+console.log("Listening to server at http://localhost:8080\n");
